d5588f6f3d693c6b84e769e3fad30d423da63c35
r-spiel <richelle.mehlhaff@gmail.com>
2020-09-16 13:05:22 -0700

    richelle's binary to decimal method

--- lib/binary_to_decimal.rb
+++ lib/binary_to_decimal.rb
@@ -5,5 +5,19 @@
 # Calculate  and return the decimal value for this binary number using
 # the algorithm you devised in class.
 def binary_to_decimal(binary_array)
-  raise NotImplementedError
+  i = 0
+  length = binary_array.length
+  decimal_notation = 0
+
+  while i < length
+
+    num = binary_array[length - 1 - i] #iterates backwards through the array
+    binary_digit_conversion = (num * (2 ** i))
+    decimal_notation += binary_digit_conversion
+    i += 1
+
+  end
+  return decimal_notation
 end
+
+
